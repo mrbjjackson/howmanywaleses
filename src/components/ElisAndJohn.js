090cf43689813a982d536dff5f110c99470d8b2a
Ben Jackson <ben@mrbjjackson.com>
2019-11-08 15:54:17 +0000

    spelling fix

--- src/components/ElisAndJohn.js
+++ src/components/ElisAndJohn.js
@@ -20,7 +20,7 @@ export default function ElisAndJohn() {
         <div className="elisAndJohn">
             <div className="thoughtBubble">
                 <h3>What's all this about then?</h3>
-                <p><strong>How Many Waleses?</strong> is a game that featured on the "Made Up Games" featured on the Elis James and John Robins BBC 5Live radio show <a href="https://www.bbc.co.uk/sounds/play/m0009t88" target="_blank" rel="noopener noreferrer">(1hr15mins into this episode).</a></p>
+                <p><strong>How Many Waleses?</strong> is a game that featured on the "Made Up Games" feature on the Elis James and John Robins BBC 5Live radio show <a href="https://www.bbc.co.uk/sounds/play/m0009t88" target="_blank" rel="noopener noreferrer">(1hr15mins into this episode).</a></p>
                 <p>The game was originally suggested by Richard in Reading and this website was created by me, <a href="https://www.instagram.com/mrbjjackson/" target="_blank" rel="noopener noreferrer">Ben Jackson</a>. I'm a music producer and web developer from Cornwall. I made it mainly because it's just a bit of fun isn't it? And also as a way to practice some new web development skills I've been teaching myself lately.</p>
             </div>
             <Img fluid={data.placeholderImage.childImageSharp.fluid} />
